dba7882f331896b6db61c60e05721b412705831e
azmil13 <21221753105@smkassalaambandung.sch.id>
2023-10-30 09:56:13 +0700

    pshh

--- index.js
+++ index.js
@@ -34,7 +34,18 @@ app.get('https://inventaris-stfi.vercel.app/barang/ruangan_id', (req, res) => {
 app.get('/ambil_data', (req, res) => {
   // Di sini Anda dapat melakukan pengambilan data dari database atau sumber data lainnya
   const data = {
-      contohData: 'Ini adalah data yang diambil dari server backend',
+    no: no,
+    nama_barang: nama_barang,
+    merek: merek,
+    tipe: tipe,
+    model: model,
+    ruangan_id: ruangan_id,
+    jumlah: jumlah,
+    tahun_peroleh: tahun_peroleh,
+    nilai_peroleh: nilai_peroleh,
+    nilai_perbaikan: nilai_perbaikan,
+    no_inventaris: no_inventaris,
+    kondisi: kondisi,
   };
 
   res.json(data);
