9dba4fbf31dc4666a1ab6d62378aeefc4631479e
azmil13 <21221753105@smkassalaambandung.sch.id>
2023-11-03 10:33:41 +0700

    push

--- index.js
+++ index.js
@@ -9,25 +9,25 @@ require('dotenv').config();
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.get('/barang', (req, res) => {
-  const { filterParameter } = req.query; // Ambil parameter filter dari query string
+// app.get('/barang', (req, res) => {
+//   const { filterParameter } = req.query; // Ambil parameter filter dari query string
 
-  let sql = 'SELECT barang.no, barang.nama_barang, barang.Merek, barang.Tipe, barang.Model, barang.Jumlah, barang.Tahun_peroleh, barang.Nilai_peroleh, barang.Nilai_perbaikan, barang.No_inventaris, barang.Kondisi, ruangan.nama_ruangan FROM barang INNER JOIN ruangan ON barang.ruangan_id = ruangan.ID_ruangan;';
+//   let sql = 'SELECT barang.no, barang.nama_barang, barang.Merek, barang.Tipe, barang.Model, barang.Jumlah, barang.Tahun_peroleh, barang.Nilai_peroleh, barang.Nilai_perbaikan, barang.No_inventaris, barang.Kondisi, ruangan.nama_ruangan FROM barang INNER JOIN ruangan ON barang.ruangan_id = ruangan.ID_ruangan;';
   
-  if (filterParameter) {
-    // Jika filterParameter diberikan, tambahkan klausa WHERE untuk filter
-    sql += ` WHERE ID_Ruangan = ${db.escape(filterParameter)}`;
-  }
+//   if (filterParameter) {
+//     // Jika filterParameter diberikan, tambahkan klausa WHERE untuk filter
+//     sql += ` WHERE ID_Ruangan = ${db.escape(filterParameter)}`;
+//   }
 
-  db.query(sql, (err, results) => {
-    if (err) {
-      console.error('Terjadi kesalahan saat mengambil data:', err);
-      res.status(500).send('Terjadi kesalahan');
-    } else {
-      res.json(results); // Kirim data yang telah difilter sebagai respons JSON
-    }
-  });
-});
+//   db.query(sql, (err, results) => {
+//     if (err) {
+//       console.error('Terjadi kesalahan saat mengambil data:', err);
+//       res.status(500).send('Terjadi kesalahan');
+//     } else {
+//       res.json(results); // Kirim data yang telah difilter sebagai respons JSON
+//     }
+//   });
+// });
 
 
 app.use((req, res, next) => {
